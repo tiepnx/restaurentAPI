5a6be8f8f4bdb87cb0681ca36e73e15339afe908
Tiep Nguyen <xuantiepnguyen@gmail.com>
2018-12-15 14:51:19 +0700

    12/15/2018

---
+++ source/RESTAURANT.API/API/FoodGroupController.cs
@@ -0,0 +1,66 @@
+﻿using RESTAURANT.API.DAL;
+using RESTAURANT.API.DAL.Services;
+using RESTAURANT.API.helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Http;
+
+namespace RESTAURANT.API.API
+{
+    [RoutePrefix("api/foodgroup")]
+    public class FoodGroupController : ApiController
+    {
+        [HttpGet]
+        [Route("items")]
+        [Authorize]
+        public IHttpActionResult GetList()
+        {
+            List<FoodGroup> items = null;
+
+            using (FoodGroupService sv = new FoodGroupService())
+            {
+                var ofs = Common.GetOFSKey(Request.GetRequestContext().Principal as ClaimsPrincipal);
+                items = sv.GetList(ofs);
+            }
+            return Ok(new { items });
+        }
+        [Authorize]
+        [HttpPost]
+        public IHttpActionResult Post(FoodGroup item)
+        {
+            int? id = null;
+            var ofs = Common.GetOFSKey(Request.GetRequestContext().Principal as ClaimsPrincipal);
+            item.OfsKey = ofs;
+            using (FoodGroupService sv = new FoodGroupService())
+            {
+                id = sv.Insert(item, HttpContext.Current.User.Identity.Name);
+            }
+            return Ok(new { id });
+        }
+        [Authorize]
+        [HttpPut]
+        public IHttpActionResult Put(FoodGroup item)
+        {
+            using (FoodGroupService svc = new FoodGroupService())
+            {
+                svc.Update(item, HttpContext.Current.User.Identity.Name);
+            }
+            return Ok(item);
+
+        }
+        [Authorize]
+        [HttpDelete]
+        public IHttpActionResult Delete(FoodGroup item)
+        {
+            using (FoodGroupService svc = new FoodGroupService())
+            {
+                svc.Delete(item, HttpContext.Current.User.Identity.Name);
+            }
+            return Ok(item);
+        }
+    }
+}
